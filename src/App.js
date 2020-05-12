c210ad808461605195553c6dd3be1a99953d8f75
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2020-05-12 15:30:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// import Header from './components/Header'
+
 import {Home} from './pages/Home'
 
 function App() {
   return (
     <Router>
+      {/* <Header/> */}
 
       <div className="App">
           <Switch>
-            <Home />
+            <Route path="/" component={Home} />
           </Switch>
       </div>
 
