3b263804a491bda3b90d0b2dd5179c829bf0083e
LonTech99 <madelon148073@hotmail.nl>
2020-06-06 20:09:28 +0200

    update tutorial

--- src/components/SwipeTutorial.js
+++ src/components/SwipeTutorial.js
@@ -10,10 +10,18 @@ export const SwipeTutorial = () => {
     
     if (visible === true) {
         return (
+
         <div class="card-tutorial">
-            <div class="card text-center">
+            <img src="../src/img/finger+hand+touch+touch+screen+icon-1320190780307921018.png" alt="..." class="img-thumbnail"></img>
+                <div class="text-center">
+                    <div class="text-tutorial">
+                        <p3>Swipe een artikel weg <br></br>
+                            om niet meer van dit soort <br></br>
+                            artikelen te zien
+                        </p3>
+                    </div>
                 
-                    <button onClick={submitHandler} class="btn btn-info">Begrepen!</button>
+                    <button onClick={submitHandler} class="understood-button">Begrepen!</button>
             </div>
         </div>
     )
