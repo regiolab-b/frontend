464e8110b1d5691b9c4e8d7dcf2ba88919a8d134
Louis Laureys <louis@laureys.me>
2020-06-08 16:57:28 +0200

    Clean up

--- src/components/SwipeTutorial.js
+++ src/components/SwipeTutorial.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import '../App.css';
-import {icoon} from '../img/icoon.png'
 
 
 export const SwipeTutorial = () => {
@@ -14,14 +13,14 @@ export const SwipeTutorial = () => {
         return (
         <div class="card-tutorial">
             <div class="container text-center">
-                <img src="https://image.flaticon.com/icons/png/512/55/55421.png" class="image mt-2"/>
+                <img src="https://image.flaticon.com/icons/png/512/55/55421.png" alt="hand swiping" class="image mt-2"/>
             </div>
             <div class="text-center">
                 <div class="text-tutorial pt-2">
-                    <p3>Swipe een artikel weg <br></br>
+                    <p>Swipe een artikel weg <br></br>
                         om minder van dat soort <br></br>
                         artikelen te zien
-                    </p3>
+                    </p>
                 </div>
                 
                 <button onClick={submitHandler} class="btn btn-outline-warning m-3">Begrepen!</button>
