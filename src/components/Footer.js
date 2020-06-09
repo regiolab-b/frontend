46d4efa7ad50cdd151c36ac4059235dcf323a0a3
YorickVdVenne <yorick.vd.venne@hotmail.nl>
2020-06-09 14:40:11 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
     <footer class="page-footer font-small bg-warning mt-3">
     
       <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://regiolab.turd.me/"> Regiolab</a>
+        <a href="https://regiolab.turd.me/"> CMGT Regiolab team B</a>
       </div>
     
     </footer>
